b82ee7e19c605cd247cfa12c8c1a2a27185ace1d
brito <brito.88@hotmail.com>
2024-12-05 13:32:04 +0000

    alteracoes product

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ProductsModule } from './products/products.module';
+import { AppController } from './app.controller';  // Adicione a importação do AppController
+import { AppService } from './app.service';  // Adicione a importação do AppService
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      omitNull: true,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
+  controllers: [AppController],  // Registre o AppController
+  providers: [AppService],  // Registre o AppService
 })
 export class AppModule {}
