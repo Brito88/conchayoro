6201cbea6aada3808121ed4381aecefd3467dcb9
Brito <brito.88@hotmail.com>
2024-12-09 19:21:10 -0300

    alterações

--- backend/src/products/products.controller.spec.ts
+++ backend/src/products/products.controller.spec.ts
@@ -1,4 +1,4 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { getModelToken } from '@nestjs/sequelize';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
@@ -6,94 +6,99 @@ import { Product } from './entities/product.entity';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
-const mockProduct = {
-  id: 1,
-  name: 'Product1',
-  price: 10,
-  category: 'C1',
-  rating: 1,
+const product = {
+    name: 'Product1',
+    price: 10,
+    category: 'C1',
+    rating: 1,
 };
 
-const mockProductArray = [mockProduct];
-
-const mockProductsService = {
-  create: jest.fn().mockResolvedValue(mockProduct),
-  findAll: jest.fn().mockResolvedValue(mockProductArray),
-  findOne: jest.fn().mockResolvedValue(mockProduct),
-  update: jest.fn().mockResolvedValue(mockProduct),
-  remove: jest.fn().mockResolvedValue(null),
-  findByCriteria: jest.fn().mockResolvedValue(mockProductArray),
-};
+const createProductDto: CreateProductDto = product;
+const updateProductDto: UpdateProductDto = product;
 
 describe('ProductsController', () => {
-  let productsController: ProductsController;
-  let productsService: ProductsService;
+    let productsService: ProductsService;
+    let productsController: ProductsController;
+    let product: Product;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [ProductsController],
-      providers: [
-        {
-          provide: ProductsService,
-          useValue: mockProductsService,
-        },
-        {
-          provide: getModelToken(Product),
-          useValue: {}, // Mock Sequelize model
-        },
-      ],
-    }).compile();
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            controllers: [ProductsController],
+            providers: [
+                ProductsService,
+                {
+                    provide: getModelToken(Product),
+                    useValue: {},
+                },
+            ],
+        }).compile();
 
-    productsController = module.get<ProductsController>(ProductsController);
-    productsService = module.get<ProductsService>(ProductsService);
-  });
+        productsService = moduleRef.get<ProductsService>(ProductsService);
+        productsController = moduleRef.get<ProductsController>(ProductsController);
+        product = moduleRef.get<Product>(getModelToken(Product));
+    });
 
-  it('should create a product', async () => {
-    const createProductDto: CreateProductDto = { name: 'Product1', price: 10, category: 'C1', rating: 1 };
+    it('should create a product', async () => {
+        const mockedResponseData = product;
+        jest
+            .spyOn(productsService, 'create')
+            .mockImplementation(() => Promise.resolve(mockedResponseData));
 
-    const result = await productsController.create(createProductDto);
+        expect(await productsController.create(createProductDto)).toBe(
+            mockedResponseData,
+        );
+    });
 
-    expect(result).toEqual(mockProduct);
-    expect(productsService.create).toHaveBeenCalledWith(createProductDto);
-  });
+    it('should update a product', async () => {
+        const mockedResponseData = product;
+        const id = '1';
+        jest
+            .spyOn(productsService, 'update')
+            .mockImplementation(() => Promise.resolve(mockedResponseData));
 
-  it('should update a product', async () => {
-    const id = '1';
-    const updateProductDto: UpdateProductDto = { ...mockProduct };
-    const result = await productsController.update(id, updateProductDto);
+        expect(await productsController.update(id, updateProductDto)).toBe(
+            mockedResponseData,
+        );
+    });
 
-    expect(result).toEqual(mockProduct);
-    expect(productsService.update).toHaveBeenCalledWith(id, updateProductDto);
-  });
+    it('should delete a product', async () => {
+        const mockedResponseData = null;
+        const id = '1';
+        jest
+            .spyOn(productsService, 'remove')
+            .mockImplementation(() => Promise.resolve(mockedResponseData));
 
-  it('should delete a product', async () => {
-    const id = '1';
-    const result = await productsController.remove(id);
+        expect(await productsController.remove(id)).toBe(mockedResponseData);
+    });
 
-    expect(result).toBeNull();
-    expect(productsService.remove).toHaveBeenCalledWith(id);
-  });
+    it('should return an array of products', async () => {
+        const mockedResponseData = [];
+        jest
+            .spyOn(productsService, 'findAll')
+            .mockImplementation(() => Promise.resolve(mockedResponseData));
 
-  it('should return an array of products', async () => {
-    const result = await productsController.findAll();
+        expect(await productsController.findAll()).toBe(mockedResponseData);
+    });
 
-    expect(result).toEqual(mockProductArray);
-    expect(productsService.findAll).toHaveBeenCalled();
-  });
+    it('should return a specific product', async () => {
+        const mockedResponseData = product;
+        const id = '1';
+        jest
+            .spyOn(productsService, 'findOne')
+            .mockImplementation(() => Promise.resolve(mockedResponseData));
 
-  it('should return a specific product', async () => {
-    const id = '1';
-    const result = await productsController.findOne(id);
+        expect(await productsController.findOne(id)).toBe(mockedResponseData);
+    });
 
-    expect(result).toEqual(mockProduct);
-    expect(productsService.findOne).toHaveBeenCalledWith(id);
-  });
+    it('should return an array of products by criteria', async () => {
+        const mockedResponseData = [];
+        const criteria = { id: '1' };
+        jest
+            .spyOn(productsService, 'findByCriteria')
+            .mockImplementation(() => Promise.resolve(mockedResponseData));
 
-  it('should return an array of products by criteria', async () => {
-    const criteria = { category: 'C1' };
-    const result = await productsController.findByCriteria(criteria);
-
-    expect(result).toEqual(mockProductArray);
-    expect(productsService.findByCriteria).toHaveBeenCalledWith(criteria);
-  });
+        expect(await productsController.findByCriteria(criteria)).toBe(
+            mockedResponseData,
+        );
+    });
 });
--- backend/src/products/products.module.spec.ts
+++ backend/src/products/products.module.spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CreateProductDto } from './dto/create-product.dto';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { SequelizeModule, SequelizeModuleOptions } from '@nestjs/sequelize';
 import { ProductsModule } from './products.module';
+import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 const databaseConnection: SequelizeModuleOptions = {
@@ -13,6 +13,20 @@ const databaseConnection: SequelizeModuleOptions = {
   synchronize: true,
 };
 
+const createProductDto: CreateProductDto = {
+  name: 'Product1',
+  price: 10,
+  category: 'C1',
+  rating: 1,
+};
+
+const updateProductDto: UpdateProductDto = {
+  name: 'Product1-updated',
+  price: 11,
+  category: 'C2',
+  rating: 2,
+};
+
 describe('ProductsModule', () => {
   let app: INestApplication;
 
@@ -20,6 +34,7 @@ describe('ProductsModule', () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [SequelizeModule.forRoot(databaseConnection), ProductsModule],
     }).compile();
+
     app = moduleFixture.createNestApplication();
     await app.init();
   });
@@ -31,20 +46,6 @@ describe('ProductsModule', () => {
       .expect('[]');
   });
 
-  const createProductDto: CreateProductDto = {
-    name: 'Product1',
-    price: 10,
-    category: 'C1',
-    rating: 1,
-  };
-
-  const updateProductDto: UpdateProductDto = {
-    name: 'Product1-updated',
-    price: 11,
-    category: 'C2',
-    rating: 2,
-  };
-
   it('POST /products', async () => {
     return request(await app.getHttpServer())
       .post('/products')
@@ -52,10 +53,10 @@ describe('ProductsModule', () => {
       .send(createProductDto)
       .expect(201)
       .expect((res) => {
-        res.body.name = createProductDto.name;
-        res.body.price = createProductDto.price;
-        res.body.category = createProductDto.category;
-        res.body.rating = createProductDto.rating;
+        expect(res.body.name).toBe(createProductDto.name);
+        expect(res.body.price).toBe(createProductDto.price);
+        expect(res.body.category).toBe(createProductDto.category);
+        expect(res.body.rating).toBe(createProductDto.rating);
       });
   });
 
@@ -65,7 +66,6 @@ describe('ProductsModule', () => {
       .expect('Content-Type', 'application/json; charset=utf-8')
       .send(createProductDto)
       .expect(201);
-
     const id = response.body.id;
 
     return request(await app.getHttpServer())
@@ -74,10 +74,10 @@ describe('ProductsModule', () => {
       .send(updateProductDto)
       .expect(200)
       .expect((res) => {
-        res.body.name = updateProductDto.name;
-        res.body.price = updateProductDto.price;
-        res.body.category = updateProductDto.category;
-        res.body.rating = updateProductDto.rating;
+        expect(res.body.name).toBe(updateProductDto.name);
+        expect(res.body.price).toBe(updateProductDto.price);
+        expect(res.body.category).toBe(updateProductDto.category);
+        expect(res.body.rating).toBe(updateProductDto.rating);
       });
   });
 
@@ -87,14 +87,19 @@ describe('ProductsModule', () => {
       .expect('Content-Type', 'application/json; charset=utf-8')
       .send(createProductDto)
       .expect(201);
-
     const id = response.body.id;
-
+  
     return request(await app.getHttpServer())
       .delete(`/products/${id}`)
       .expect(200)
       .expect((res) => {
-        res.body = null;
+        // Verificar se o corpo da resposta é um objeto vazio
+        expect(res.body).toEqual({});
       });
+  });
+  
+
+  afterAll(async () => {
+    await app.close();
   });
 });
